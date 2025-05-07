a4287a37959cfc9b856e391c45e9c383ac7ff933
fede <fe.pereira@duocuc.cl>
2025-05-07 17:03:36 -0400

    Re estructuracion para BD

--- proyect/src/main/java/duoc/proyect/model/Profesor.java
+++ proyect/src/main/java/duoc/proyect/model/Profesor.java
@@ -1,5 +1,9 @@
 package duoc.proyect.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Entity
+
 public class Profesor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
     private String Name;
+    @Column(unique=true)
     private String Email;
 }
--- proyect/src/main/java/duoc/proyect/service/ProfesorService.java
+++ proyect/src/main/java/duoc/proyect/service/ProfesorService.java
@@ -13,13 +13,53 @@ public class ProfesorService {
     @Autowired
     ProfesorRepository profesorRepository;
 
-    public List<Profesor> getAllProfesors(){return profesorRepository.getAllProfesores();}
+    public String getProfesores(){
+        String output = "";
+        for(Profesor profesor : profesorRepository.findAll()){
+            output += profesor.toString() + "\n";
+        }
+        if(output.isEmpty()){
+            return "No hay profesores.";
+        }else {
+            return output;
+        }
+    }
 
-    public Profesor getProfesorById(int id){return profesorRepository.getProfesorById(id);}
+    public String addProfesor(Profesor profesor){
+        profesorRepository.save(profesor);
+        return "Profesor agregado";
+    }
 
-    public String addProfesor(Profesor Profesor){return profesorRepository.addProfesor(Profesor);}
+    public String getProfesorById(int id){
+        for (Profesor profesor : profesorRepository.findAll()) {
+            if (profesor.getId() == id) {
+                return profesor.toString();
+            }
+        };
+        return "Profesor no encontrado";
+    }
 
-    public String deleteProfesor(int id){return profesorRepository.deleteProfesor(id);}
+    public String deleteProfesor(int id){
+        if(profesorRepository.existsById(id)){
+            profesorRepository.deleteById(id);
+            return "Profesor eliminado";
+        }else {
+            return "Profesor no encontrado";
+        }
+    }
 
-    public String updateProfesor(Profesor a){return profesorRepository.updateProfesor(a);}
+    public String updateProfesor(Profesor newProfesor, int id){
+        if(profesorRepository.existsById(id)){
+            for(Profesor profesor : profesorRepository.findAll()){
+                if(profesor.getId() == id){
+                    profesor.setName(newProfesor.getName());
+                    profesor.setEmail(newProfesor.getEmail());
+                    profesorRepository.save(profesor);
+                }
+            }
+            return "Profesor actualizado "+newProfesor.toString();
+        }else{
+            return "Profesor no encontrado";
+        }
+    }
 }
