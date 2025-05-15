310bc3b5feaa8179e0bce67d793536cf3a40a81e
fede <fe.pereira@duocuc.cl>
2025-05-15 15:34:12 -0400

    herencia de clase usuario mejorada con supertipo

--- proyect/src/main/java/duoc/proyect/model/Usuario.java
+++ proyect/src/main/java/duoc/proyect/model/Usuario.java
@@ -1,26 +1,22 @@
 package duoc.proyect.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import lombok.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "tipo_usuario")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
-    @Column(unique=true)
+    @Column(unique = true, nullable = false)
     private String rut;
+
     private String name;
     private String lastName;
     private String mail;
 }
-
